c2975a32a51c1b8e71ee5504726d2c36fbf8a6ad
natekcroteau <natekcroteau@gmail.com>
2021-03-08 11:41:59 -0700

    working gratitude entry model/controller/routes

--- app/controllers/gratitudes_controller.rb
+++ app/controllers/gratitudes_controller.rb
@@ -2,7 +2,18 @@ class GratitudesController < ApplicationController
 
   def index
     @gratitudes = Gratitude.all
-    render.json: @gratitudes
+    render json: @gratitudes
+  end
+
+  def create 
+    @gratitude = Gratitude.create(gratitude_params)
+    render json: @gratitude
+  end
+
+private
+
+  def gratitude_params
+    params.require(:gratitudes).permit(:date, :user_id, :entry1, :entry2, :entry3, :entry4, :entry5, :entry6,)
   end
 
 end
