b27bd42ab8673834efe0f2c8673478fd3406bafa
natekcroteau <natekcroteau@gmail.com>
2021-03-08 11:48:51 -0700

    working freewrite model/controller/routes

--- app/controllers/freewrites_controller.rb
+++ app/controllers/freewrites_controller.rb
@@ -1,8 +1,22 @@
 class FreewritesController < ApplicationController
 
+
   def index
     @freewrites = Freewrite.all
-    render.json: @freewrites
+    render json: @freewrites
   end
 
+  def create 
+    @freewrite = Freewrite.create(freewrite_params)
+    render json: @freewrite
+  end
+
+private
+
+  def freewrite_params
+    params.require(:freewrites).permit(:date, :time, :entry, :user_id)
+  end
+
+
 end
+
